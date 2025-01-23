4c2c879ffba05650aad87aa0d1c1f664c16aead1
NAIMJAE <skdlawo5985@gamil.com>
2025-01-24 00:55:50 +0900

    inti

--- src/main/java/com/pingo/controller/KeywordController.java
+++ src/main/java/com/pingo/controller/KeywordController.java
@@ -14,8 +14,9 @@ public class KeywordController {
 
     final private KeywordService keywordService;
 
+    // 2차 키워드 카테고리까지 조회 for Keyword_Page
     @GetMapping("/keyword")
     public ResponseEntity<?> selectKeyword() {
-        return keywordService.selectKeywordListForCategory();
+        return keywordService.selectKeywordListFor2ndCategory();
     }
 }
