a4e40df464494bab59052d9ce0e4ba23ec2ab5fe
naimjae <skdlawo5985@gmail.com>
2025-02-03 12:16:12 +0900

    a

--- src/main/java/com/pingo/controller/SignController.java
+++ src/main/java/com/pingo/controller/SignController.java
@@ -5,6 +5,7 @@ import com.pingo.dto.UserSignUp;
 import com.pingo.service.signService.SignService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
