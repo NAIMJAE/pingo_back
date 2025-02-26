57c0951a1de0be4453eb908cafb50a079995d2df
naimjae <skdlawo5985@gmail.com>
2025-02-26 17:27:58 +0900

    결제 완료

--- src/main/java/com/pingo/service/membershipService/MembershipService.java
+++ src/main/java/com/pingo/service/membershipService/MembershipService.java
@@ -3,6 +3,8 @@ package com.pingo.service.membershipService;
 import com.pingo.dto.ResponseDTO;
 import com.pingo.entity.membership.Membership;
 import com.pingo.entity.membership.UserMembership;
+import com.pingo.exception.BusinessException;
+import com.pingo.exception.ExceptionCode;
 import com.pingo.mapper.MembershipMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +35,27 @@ public class MembershipService {
         }
 
         return ResponseEntity.ok().body(ResponseDTO.of("1", "성공", resultMap));
+    }
+
+    // 멤버쉽 등록
+    public ResponseEntity<?> setMembership(String msNo, String userNo) {
+
+        try {
+            Optional<UserMembership> userMembership = membershipMapper.selectUserMembership(userNo);
+
+            if (userMembership.isPresent()) {
+                throw new BusinessException(ExceptionCode.DUPLICATE_MEMBERSHIP);
+            }
+
+            UserMembership newMembership = new UserMembership();
+            newMembership.createMembershipInfo(msNo, userNo);
+
+            membershipMapper.insertUserMembership(newMembership);
+
+            return ResponseEntity.ok().body(ResponseDTO.of("1","성공",newMembership.getExpDate().toString()));
+        } catch (Exception e) {
+            throw new BusinessException(ExceptionCode.MEMBERSHIP_PROCESS_FAIL);
+        }
     }
 
 }
